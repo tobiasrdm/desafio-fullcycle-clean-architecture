c292114475f1d4b446aca9caf355f69d0f7ea330
Tobias Ricken de Medeiros <tobiasrdm@gmail.com>
2025-08-20 15:25:25 +0000

    Desafios 3 e 4: Notification Pattern e Validação em Products

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -10,14 +10,20 @@ describe("Product unit tests", () => {
   it("should throw error when name is empty", () => {
     expect(() => {
       const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+    }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    }).toThrowError("product: Price must be greater than zero");
   });
+
+  it("should throw error when name is and id are empty and price is less than zero", () => {
+      expect(() => {
+        const product = new Product("", "", -1);
+      }).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than zero");
+    });
 
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
